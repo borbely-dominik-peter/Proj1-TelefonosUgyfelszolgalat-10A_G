c17a4507a09a8ad5d2b18cc25273160eb41c0a53
borbely-dominik-peter <98750586+borbely-dominik-peter@users.noreply.github.com>
2023-02-08 09:37:35 +0100

    Update Megoldas.py

--- Megoldas.py
+++ Megoldas.py
@@ -15,3 +15,14 @@ class Megoldas:
 
     def mpbe(self, hour: int, minute: int, second: int):
         return hour * 3600 + minute * 60 + second
+    
+    def hivas_hossz(self, first_hour: int, first_minute: int, first_second: int, last_hour: int, last_minute: int, last_second: int):
+        hossz: int = 0
+        hossz = self.mpbe(last_hour, last_minute, last_second) - self.mpbe(first_hour, first_minute, first_second)
+        return hossz
+    
+    def leghosszabb_hivas(self):
+        for e in self.phone_calls:
+            self.hivas_hossz(e.First_hour, e.First_min, e.First_sec, e.Last_hour, e.Last_min, e.Last_sec)
+
+
