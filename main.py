54d259009aba26a6b1924e150f40fcf68ccacc9d
borbely-dominik-peter <98750586+borbely-dominik-peter@users.noreply.github.com>
2023-02-15 09:36:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,16 +16,7 @@ def main() -> None:
         all_num = 0
 
     print('4. Feladat')
-    hivas_hossz: int = 0
-    sor_szamlalo: int = 0
-    for e in phone.phone_calls:
-        if e.hivas_hossz > hivas_hossz:
-            hivas_hossz = e.hivas_hossz
-    for e in phone.phone_calls:
-        sor_szamlalo += 1
-        if e.hivas_hossz == hivas_hossz:
-            break
-    print(f'A leghosszabb ideig vonalban lévő hívó {sor_szamlalo} sorban szerepel, a hívás hossza: {hivas_hossz} másodperc.')
+    print(f'A leghosszabb ideig vonalban lévő hívó {phone.longest_call_line} sorban szerepel, a hívás hossza: {phone.longest_call_length} másodperc.')
 
     print("5. Feladat")
     input_numbers: str = input("Adjon meg egy idopontot! (ora perc masodperc) ")
@@ -33,6 +24,11 @@ def main() -> None:
     # print(phone.waiting_people_num(input_numbers))
     # print(f'A várakozók száma: {phone.waiting_people_num(input_numbers)} a beszélo a {phone.accepted_caller_num(input_numbers)}. hívó.')
     print(f'{"Nem volt beszélő" if phone.waiting_people_num(input_numbers) == 0 else f"A várakozók száma: {phone.waiting_people_num(input_numbers)} a beszélo a {phone.accepted_caller_num(input_numbers)}. hívó."}')
+    
+    print("6. Feladat")
+    print(phone.last_caller_num)
+    print(phone.call_for_all_hour)
+    # print(f'Az utolso telefonalo adatai a(z) {}. sorban vannak, {} masodpercig vart.')
 
 
 if __name__ == "__main__":
