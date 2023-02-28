7afe0d0ae02df7801d5694dd0ffdf984f161ac12
borbely-dominik-peter <98750586+borbely-dominik-peter@users.noreply.github.com>
2023-02-28 18:47:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,27 +3,24 @@ from Megoldas import Megoldas
 
 def main() -> None:
     # beolvasás
-    phone: Megoldas = Megoldas('hivastest.txt')
+    phone: Megoldas = Megoldas('hivas.txt')
     # print(len(phone.phone_calls))  # test hogy működik e a inicializálás
 
     print('3. feladat')
-    all_num: int = 0
-    for number in range(8, 12):
-        for e in phone.phone_calls:
-            if e.First_hour == number:
-                all_num += 1
-        print(f'{number} óra {all_num} hívás')
-        all_num = 0
+    print(phone.stat_hours_print)
 
     print('4. Feladat')
     print(f'A leghosszabb ideig vonalban lévő hívó {phone.longest_call_line} sorban szerepel, a hívás hossza: {phone.longest_call_length} másodperc.')
 
     print("5. Feladat")
     input_numbers: str = input("Adjon meg egy idopontot! (ora perc masodperc) ")
-    print(f'{"Nem volt beszélő" if phone.waiting_people_num(input_numbers) == 0 else f"A várakozók száma: {phone.waiting_people_num(input_numbers)} a beszélo a {phone.accepted_caller_num(input_numbers)}. hívó."}')
+    print(f'{"Nem volt beszélő" if phone.waiting_people_num(input_numbers) <= 0 else f"A várakozók száma: {phone.waiting_people_num(input_numbers)} a beszélo a {phone.accepted_caller_num(input_numbers)}. hívó."}')
 
     print("6. Feladat")
-    print(f'Az utolso telefonalo adatai a(z) {phone.call_for_all_hour}. sorban vannak, {phone.last_caller_wait_length} masodpercig vart.')
+    print(f'Az utolso telefonalo adatai a(z) {phone.last_accepted_call_line}. sorban vannak, {phone.last_caller_waiting_time} masodpercig vart.')
+
+    print("7. Feladat")
+    print(phone.accepted_callers_text)
 
 
 if __name__ == "__main__":
